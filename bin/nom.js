aee37979d1984d386777f92a30b198125d2bd5f2
Alex <16batesa@gmail.com>
2016-12-09 16:47:09 +0000

    FIx nom update... again again

--- bin/nom.js
+++ bin/nom.js
@@ -59,8 +59,8 @@ if (command === 'update') needsUpdateCheck = false
     request('https://raw.githubusercontent.com/nanalan/nom/master/install.sh', (err, res, body) => {
       if (!err && res.statusCode == 200) {
         require('child_process').execSync('rm -rf ~/.nom', { stdio: 'inherit' })
-        fs.writeFileSync('~/.nom.sh', body, 'utf8')
-        require('child_process').execSync('sh .nom.sh && rm -rf .nom.sh', {
+        fs.writeFileSync(`${process.env.HOME}/.nom.sh`, body, 'utf8')
+        require('child_process').execSync('sh ~/.nom.sh && rm -rf ~/.nom.sh', {
           stdio: 'inherit'
         })
       } else console.error(chalk.red(err))
